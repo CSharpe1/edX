6ef3e8e526539d7cdedbead077b89697a4f76b2c
charles sharpe <charles.sharpe@meteorinkjet.com>
2020-01-13 19:03:11 +0000

    Init

---
+++ CS50/exit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (agrc != 2)
+    {
+        printf("Missing comand-line argument\n")
+        return 1;
+    }
+    printf("Hello, %s\n", argv[1]);
+    return 0;
+}
--- Harvard_CS50/CS50-edx/int.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-
-int main(void)
-{
-    int i = get_int("integer: ");
-    printf("hello", %i \n,i);
-}
