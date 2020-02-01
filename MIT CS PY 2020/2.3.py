3cb4da758c5e0e54485502d27f72a11638b75b82
charles sharpe <charles.sharpe@meteorinkjet.com>
2020-02-01 17:22:48 +0000

    Create 2.3.py

---
+++ MIT CS PY 2020/2.3.py
@@ -0,0 +1,12 @@
+balance = 320000
+annualInterestRate = 0.2
+#	Lowest Payment: 29157.09
+
+
+Monthly_interest_rate = (Annual_interest_rate) / 12.0
+Monthly_payment_lower_bound = Balance / 12
+Monthly_payment_upper_bound = (Balance * (1 + Monthly_interest_rate)	12)	/	12
+
+
+
+
