0fbe83e62da8baf98ac9bfc74e3e1281f3856208
charles sharpe <charles.sharpe@meteorinkjet.com>
2020-02-02 18:24:25 +0000

    .

--- MIT CS PY 2020/2.3.py
+++ MIT CS PY 2020/2.3.py
@@ -5,8 +5,19 @@ annualInterestRate = 0.2
 
 Monthly_interest_rate = (Annual_interest_rate) / 12.0
 Monthly_payment_lower_bound = Balance / 12
-Monthly_payment_upper_bound = (Balance * (1 + Monthly_interest_rate)	12)	/	12
+Monthly_payment_upper_bound = (Balance * (1 + Monthly_interest_rate)**12)	/	12
 
+epsilon = 0.01
+
+Updated_balance_each_month = balance
+
+while abs(Updated_balance_each_month) > epsilon:
+    minimumpayment = (lower_bound + upper_bound) / 2
+    Updated_balance_each_month = balance
+	while month < 13:
+
+
+###############
 
 
 
