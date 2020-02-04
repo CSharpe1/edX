4b3ee392dbe8894a253eaeb45020d789c8cd673f
charles sharpe <charles.sharpe@meteorinkjet.com>
2020-02-04 14:17:45 +0000

    Did some more

--- MIT CS PY 2020/1.3.py
+++ MIT CS PY 2020/1.3.py
@@ -7,28 +7,69 @@
 #Longest substring in alphabetical order is: abc
 
 
-s = 'abcxyz'
+s = 'abcaxyz'
 #######
-Longsubstring = 'a'
 length = len(s) - 1
 Num = 0
 it1 = 0
 it2 = 1
 stringlength = 0
 
+
+count = 0
+maxcount = 0
+result = 0
+
+for i in range(len(s) - 1):
+    if (s[i] <= s[i + 1]):
+        count += 1
+        if count > maxcount:
+            maxcount = count
+            result = i + 1
+    else:
+        count = 0
+startposition = result - maxcount
+print('Longest substring in alphabetical order is:', s[startposition:result + 1])
+
+# Correct
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####
 #print(s[0:1]);
 #print(s[1:2]);
 
 #a = s[0:1]
 #b = s[1:2]
 
-while Num < length:
-    if  s[it1] < s[it1+1]:            #s[it1:it2] < s[it2:it2+1] :
-        stringlength += 1
+# while Num < length:
+    # if  s[it1] < s[it1+1]:            #s[it1:it2] < s[it2:it2+1] :
+        # stringlength += 1
 #        it1 += 1
-        print("smaller than previous", s[stringlength-1]);
-    else:
-        print("bigger than previous")
+        # print("smaller than previous", s[stringlength-1]);
+    # else:
+        # print("bigger than previous")
 
 
 
@@ -38,4 +79,4 @@ while Num < length:
 #if (s[1:1]) > (s[2:2]):
 #    print("Bigger")
 
-print("Longest substring in alphabetical order is: ", Longsubstring);
+#print("Longest substring in alphabetical order is: ", Longsubstring);
--- MIT CS PY 2020/w2_Guess my number.py
+++ MIT CS PY 2020/w2_Guess my number.py
@@ -10,33 +10,68 @@
 #######################
 
 
+# x = 50
+
+# step = 1
+
+# H_Limit	=	100
+# L_Limit	= 	1
+
+# print("Please think of a number between 0 and 100!");
+# print("is your secret number", x);
+# print("?");
+# print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.");
+# User_Responce = input()
+# while User_Responce != 'c':
+	# print("Is your secret number ", x);
+	# print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.");
+	# User_Responce	=	input()
+	# if 	User_Responce == 'l':
+# 		CODE TO make the guess Larger		
+		# x = round((x+((H_Limit - x)/2)))
+	# if	User_Responce == 'h':
+		# print("That was too High");
+# 		CODE to make the Guess Smaller
+		# x = round((x-((H_Limit - x)/2)))
+		# print("x is now", x);
+
+		
+		
+	# if User_Responce == 'c'	:
+		# print("Game over. Your secret number was: ",x);
+
+
+
 x = 50
 
 step = 1
-
 H_Limit	=	100
-L_Limit	= 	1
+L_Limit	= 	0
 
-print("Please think of a number between 0 and 100!");
-print("is your number", x);
-print("?");
-print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.");
+#print("Please think of a number between 0 and 100!");
+#print("Is your secret number", x, end='');
+#print("?");
+#print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.", end='');
 User_Responce = input()
 while User_Responce != 'c':
 	print("Is your secret number ", x);
-	print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.");
+	print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.", end='');
 	User_Responce	=	input()
-	if 	User_Responce == 'l':
-## 		CODE TO make the guess Larger		
-		x = round((x+((H_Limit - x)/2)))
-	if	User_Responce == 'h':
-		print("That was too High");
-## 		CODE to make the Guess Smaller
-		x = round((x-((H_Limit - x)/2)))
-		print("x is now", x);
 
-		
-		
+
 	if User_Responce == 'c'	:
-		print("Game over. Your secret number was: ",x);
+		print("Game over. Your secret number was: ",x, end='');
 
+
+	elif 	User_Responce == 'l':
+## 		CODE TO make the guess Larger
+		L_Limit == User_Responce
+		x = round((x+((H_Limit - x)//2)))
+	elif	User_Responce == 'h':
+		print("That was too High", end='');
+## 		CODE to make the Guess Smaller
+		H_Limit == User_Responce
+		x = round((x-((H_Limit - x)//2)))
+		print("x is now", x, end='');
+	else:
+		print("Sorry, I did not understand your input.")
