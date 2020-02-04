6b50939ad2e3ec797bd26e6540b38119510338d4
charles sharpe <charles.sharpe@meteorinkjet.com>
2020-02-04 16:35:50 +0000

    Update w2_Guess my number.py

--- MIT CS PY 2020/w2_Guess my number.py
+++ MIT CS PY 2020/w2_Guess my number.py
@@ -47,31 +47,22 @@ x = 50
 step = 1
 H_Limit	=	100
 L_Limit	= 	0
+Guess = False
 
-#print("Please think of a number between 0 and 100!");
-#print("Is your secret number", x, end='');
-#print("?");
-#print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.", end='');
-User_Responce = input()
-while User_Responce != 'c':
-	print("Is your secret number ", x);
-	print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.", end='');
-	User_Responce	=	input()
+print("Please think of a number between 0 and 100!");
 
-
+while not Guess: # != 'c':
+	x = (H_Limit+L_Limit)//2
+	print("Is your secret number " + str(x) + "?");  #, x,"?");
+	User_Responce	=	input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
 	if User_Responce == 'c'	:
 		print("Game over. Your secret number was: ",x, end='');
-
-
+		Guess = True
+	elif	User_Responce == 'h':
+## 		CODE to make the Guess Smaller
+		H_Limit = x
 	elif 	User_Responce == 'l':
 ## 		CODE TO make the guess Larger
-		L_Limit == User_Responce
-		x = round((x+((H_Limit - x)//2)))
-	elif	User_Responce == 'h':
-		print("That was too High", end='');
-## 		CODE to make the Guess Smaller
-		H_Limit == User_Responce
-		x = round((x-((H_Limit - x)//2)))
-		print("x is now", x, end='');
+		L_Limit = x
 	else:
 		print("Sorry, I did not understand your input.")
